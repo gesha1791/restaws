f2313f1f4be8192de004d35a380f3535b2821b24
teammember <geshachapik@gmail.com>
2021-02-12 15:22:42 +0200

    init

---
+++ src/main/java/dag/restaws/RestawsApplication.java
@@ -0,0 +1,13 @@
+package dag.restaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestawsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dag/restaws/RestawsApplicationTests.java
@@ -0,0 +1,13 @@
+package dag.restaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
